5abddf0fe6cc66240d9cb214cc9e1ca2c31e8a00
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-21 00:32:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+
+
+document.getElementById("btn").addEventListener("click", getDelayed)
+
+async function getDelayed(){
+	let text = document.getElementById("text").value;
+	let delay = document.getElementById("delay").value;
+	let screen = document.getElementById("output");
+	
+	screen.innerHTML = "Waiting!..."
+
+	await new Promise(resolve => setTimeout(resolve, delay));
+	screen.innerHTML = text;
+}
